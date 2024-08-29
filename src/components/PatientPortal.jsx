e28eb7e9ac115d84caac2a3e9bfb514761e97cfb
ashraf9932 <ashraf.neizk@gmail.com>
2024-08-29 23:46:13 +0300

    Add Appointments to Patient Portal page

--- src/components/PatientPortal.jsx
+++ src/components/PatientPortal.jsx
@@ -14,9 +14,10 @@ const PatientPortal = () => {
                     <div key={patient.id} className='patient-card'>
                         <h4>{patient.name}</h4>
                         <h6>ID: {patient.id}</h6>
+                        <h6>Gender: {patient.gender}</h6>
                         <ul>
                             {appointments
-                                .filter(app => app.patientId === patient.id)
+                                .filter(app => app.patientId.toString() === patient.id.toString())
                                 .map(app => (
                                     <li key={app.id} className='appointment-item'>
                                         {app.date} - {app.time} : {app.reason}
